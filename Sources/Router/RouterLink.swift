8c36b8a861e8abba84587c8849a88d2961e105c6
Carson Katri <carson.katri@gmail.com>
2020-06-03 21:53:28 -0400

    Add plain style to RouterLink

--- Sources/Router/RouterLink.swift
+++ Sources/Router/RouterLink.swift
@@ -11,21 +11,38 @@ import SwiftWebUI
 public struct RouterLink<Content: View>: View {
     public let destination: String
     public let content: Content
+    public let style: Style
     
-    public init(to destination: String, @ViewBuilder _ content: () -> Content) {
+    public init(to destination: String, style: Style = .button, @ViewBuilder _ content: () -> Content) {
         self.destination = destination
         self.content = content()
+        self.style = style
     }
 
-    public init(to destination: [String], @ViewBuilder _ content: () -> Content) {
-        self.init(to: destination.joined(separator: "/"), content)
+    public init(to destination: [String], style: Style = .button, @ViewBuilder _ content: () -> Content) {
+        self.init(to: destination.joined(separator: "/"), style: style, content)
     }
 
     public var body: some View {
-        Button(action: {
-            Navigator.navigateTo(destination)
-        }) {
-            content
+        Group {
+            if style == .button {
+                Button(action: self.navigate) {
+                    content
+                }
+            } else if style == .plain {
+                content.onTapGesture(self.navigate)
+            } else {
+                Text("")
+            }
         }
+    }
+    
+    func navigate() {
+        Navigator.navigateTo(destination)
+    }
+    
+    public enum Style {
+        case button
+        case plain
     }
 }
